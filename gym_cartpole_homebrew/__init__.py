e3e5306bd3ed09cd4494e96cd8559e8ca771b8dd
spiazza1297 <32577043+spiazza1297@users.noreply.github.com>
2019-10-25 07:08:12 -0400

    Update __init__.py

--- gym_cartpole_homebrew/__init__.py
+++ gym_cartpole_homebrew/__init__.py
@@ -1,7 +1,7 @@
 from gym.envs.registration import register
 
 register(
-    id='cartpole-homebrew',
+    id='cartpole_homebrew-v0',
     entry_point='gym_cartpole_homebrew.envs:CartpoleHomebrewEnv',
 )
 #register(
