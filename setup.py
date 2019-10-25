99c3bea55ab33226811f7fd0f43ccb73e121e860
spiazza1297 <32577043+spiazza1297@users.noreply.github.com>
2019-10-25 17:21:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gym_cartpole_homebrew',
-      packages=['gym_cartpole_homebrew'],
+      packages=find_packages(),
       version='0.0.1',
       install_requires=['gym']  # And any other dependencies cartpole_homebrew needs
 )
