666aae81b167510a262675d37ccd20973c77f10e
spiazza1297 <32577043+spiazza1297@users.noreply.github.com>
2019-10-25 18:47:06 -0400

    Update cartpole_homebrew_env.py

--- gym_cartpole_homebrew/envs/cartpole_homebrew_env.py
+++ gym_cartpole_homebrew/envs/cartpole_homebrew_env.py
@@ -10,7 +10,7 @@ from gym import spaces, logger
 from gym.utils import seeding
 import numpy as np
 
-class CartPoleEnv(gym.Env):
+class CartpoleHomebrew(gym.Env):
     """
     Description:
         A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track. The pendulum starts upright, and the goal is to prevent it from falling over by increasing and reducing the cart's velocity.
