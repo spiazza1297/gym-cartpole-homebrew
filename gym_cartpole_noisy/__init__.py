e0a53e073b0c4cc14f167156d4796c0e6d3488ea
spiazza1297 <32577043+spiazza1297@users.noreply.github.com>
2020-01-19 13:26:06 -0500

    Update __init__.py

--- gym_cartpole_noisy/__init__.py
+++ gym_cartpole_noisy/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 register(
     id='CartpoleNoisy-v0',
-    entry_point='gym_cartpole_noisy.envs:CartpoleNoisyEnv',
+    entry_point='gym_cartpole_noisy.envs:CartPoleNoisyEnv',
 )
 #register(
 #    id='foo-extrahard-v0',
