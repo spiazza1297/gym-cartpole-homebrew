0dd2c72b471d484a2f165c8ef73801bdc45b1501
spiazza1297 <32577043+spiazza1297@users.noreply.github.com>
2020-01-19 11:50:23 -0500

    Update __init__.py

--- gym_cartpole_homebrew/envs/__init__.py
+++ gym_cartpole_homebrew/envs/__init__.py
@@ -1,2 +1,2 @@
-from gym_cartpole_homebrew.envs.cartpole_homebrew_env import CartpoleHomebrewEnv
+from gym_cartpole_noisy.envs.cartpole_noisy_env import CartpoleNoisyEnv
 #from gym_foo.envs.foo_extrahard_env import FooExtraHardEnv
