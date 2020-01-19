a377ebd012a33a2c7a8ae449bf3751ac5a0d8611
spiazza1297 <32577043+spiazza1297@users.noreply.github.com>
2020-01-19 14:10:30 -0500

    Update __init__.py

--- gym_cartpole_noisy/envs/__init__.py
+++ gym_cartpole_noisy/envs/__init__.py
@@ -1,2 +1,2 @@
-from gym_cartpole_noisy.envs.cartpole_noisy_env import CartpoleNoisyEnv
+from gym_cartpole_noisy.envs.cartpole_noisy_env import CartPoleNoisyEnv
 #from gym_foo.envs.foo_extrahard_env import FooExtraHardEnv
